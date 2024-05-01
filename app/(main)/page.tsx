31382f02d88ed6aee7013ce8468ba720ac17b9f1
hejzbh <hazimba123@gmail.com>
2024-05-01 20:53:36 +0100

    edit

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,10 @@
-export default function HomePage() {
+import { auth } from "@clerk/nextjs/server";
+
+export default async function HomePage() {
+  const user = auth();
   return (
     <main>
-      <h1>Hello</h1>
+      <h1>User ID: {user?.userId}</h1>
     </main>
   );
 }
