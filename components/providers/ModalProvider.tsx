abc29805c8cc09227cf6247629d465b5f8e63129
hejzbh <hazimba123@gmail.com>
2024-05-03 20:03:22 +0100

    update

--- components/providers/ModalProvider.tsx
+++ components/providers/ModalProvider.tsx
@@ -4,12 +4,33 @@ import React from "react";
 import dynamic from "next/dynamic";
 import { useModal } from "@/store/modal-store";
 const Modal = dynamic(() => import("@/components/modals/Modal"));
-const Loader = dynamic(() => import("@/components/ui/Loader"));
+const UploadModal = dynamic(() => import("@/components/modals/UploadModal"));
 
 // Props
 interface ModalProviderProps {}
 
 const ModalProvider = ({}: ModalProviderProps) => {
+  const { modalType, isModalOpened } = useModal();
+
+  if (!isModalOpened) return null;
+
+  switch (modalType) {
+    case "upload":
+      return (
+        <Modal>
+          <UploadModal />
+        </Modal>
+      );
+    default:
+      return null;
+  }
+};
+
+export default ModalProvider;
+
+// KOD UKOLIKO U BUDUCNOSTI BUDE VISE MODALA
+
+/**const ModalProvider = ({}: ModalProviderProps) => {
   const { modalType, isModalOpened } = useModal();
 
   if (!isModalOpened) return null;
@@ -31,5 +52,4 @@ const ModalProvider = ({}: ModalProviderProps) => {
       return null;
   }
 };
-
-export default ModalProvider;
+ */
