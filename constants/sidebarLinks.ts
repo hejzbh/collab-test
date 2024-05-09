e9eb4530031776b5e009c3fb28099ce0cedf005e
hejzbh <hazimba123@gmail.com>
2024-05-09 15:08:22 +0100

    edit

--- constants/sidebarLinks.ts
+++ constants/sidebarLinks.ts
@@ -1,8 +1,6 @@
 import { SidebarLinksType } from "@/types";
-import { VideoIcon, CameraIcon, UploadIcon } from "lucide-react";
+import { VideoIcon } from "lucide-react";
 
 export const sidebarLinks: SidebarLinksType = [
   { title: "Home", href: "/", Icon: VideoIcon },
-  { title: "Your Clips", href: "/clips", Icon: CameraIcon },
-  { title: "Upload", href: "/upload", Icon: UploadIcon },
 ];
