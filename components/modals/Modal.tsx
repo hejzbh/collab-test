ca5198df553b757e487aeadb084d2fdf513692b6
hejzbh <hazimba123@gmail.com>
2024-05-15 18:35:07 +0100

    edit

--- components/modals/Modal.tsx
+++ components/modals/Modal.tsx
@@ -20,7 +20,7 @@ const Modal = ({ children }: ModalProps) => {
   return (
     <div
       onClick={onModalClose}
-      className="fixed top-0 left-0 w-full h-full z-20 bg-black/90 flex items-center justify-center"
+      className="fixed top-0 left-0 w-full h-full z-[2000] bg-black/90 flex items-center justify-center"
     >
       <main className="p-7 bg-bgColors-modal rounded-xl shadow-xl w-[330px] sm:w-[550px]  h-[600px] md:w-[600px] md:h-[600px] flex items-center justify-center">
         {children}
